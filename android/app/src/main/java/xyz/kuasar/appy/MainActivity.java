95c045d911e8891fb5c8b053f6520fb79bc114ac
Samuel <samuel@mknd-records.com>
2024-03-25 13:48:34 +0100

    Ajout iOS & Android support

---
+++ android/app/src/main/java/xyz/kuasar/appy/MainActivity.java
@@ -0,0 +1,5 @@
+package xyz.kuasar.appy;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'xyz.kuasar.appy',
   appName: 'AppY',
   webDir: 'dist',
   server: {
